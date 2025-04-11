569fd28a51c64befc6f7a2adcf782470b470ce25
Loredana <lmtnvc@gmail.com>
2025-04-11 18:55:00 +0200

    prvi put

---
+++ src/Window.java
@@ -0,0 +1,46 @@
+public class Window {
+
+    String naslov;
+    String oznaka;
+    String x1;
+    String x2;
+    String x3;
+    String x4;
+    String boja;
+
+    static void aktivan() {
+    }
+
+    static void width() {
+    }
+
+    static void height() {
+    }
+
+    public Window(String naslov) {
+        naslov = this.naslov;
+    }
+
+    public String getNaslov() {
+        return this.naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    public void area() {
+    }
+
+    public void perimeter() {
+    }
+
+    public void draw() {
+            }
+
+
+}
+
+
+
+
